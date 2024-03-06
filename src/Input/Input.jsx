bc248350c9e108b085558a56a6e29f424f1fabbd
Mairo Dronsart <mairo.dronsart@csgroup.eu>
2024-03-06 19:06:53 +0100

    Default Input

--- src/Input/Input.jsx
+++ src/Input/Input.jsx
@@ -1,7 +1,50 @@
+import { useState } from 'react';
+import './input.css';
+
 export const Input = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(props.options[0]);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const onOptionClick = (value) => () => {
+     setSelectedOption(value);
+     setIsOpen(false);
+  };
+
   return (
     <>
-    {props.select ? <select name="select" id="select"></select> : <input type="text" />}
+    {props.label && (
+      <label htmlFor="custom-select">{props.label}</label>
+    )}
+    {props.select ? 
+      <div 
+        style={{ 
+          border: isOpen ? '1px solid rgba(60, 120, 238, 1)' : '',
+          borderRadius: isOpen ? '8px 8px 0px 0px' : ''
+        }} 
+        className="custom-select" onClick={toggleOpen}>
+        <div className="selected-option">
+          {selectedOption}
+        </div>
+        {isOpen && (
+          <div className="options">
+            {props.options.map((option) => (
+              <div key={option} className="option" onClick={onOptionClick(option)}>
+                {option}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      : 
+      <input 
+        type="text" 
+        placeholder={props.placeholder}
+      />
+    }
     </>
   )
 }
